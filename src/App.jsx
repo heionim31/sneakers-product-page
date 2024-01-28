af815518f33bc9b6f9d3a5a4ccd85b1dd2fff78b
heion31 <mainit.noah3113@gmail.com>
2024-01-28 13:26:20 +0800

    setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
-  return <h1>Hello Heionim! ☠️</h1>;
+  return (
+    <h1 className="flex justify-center text-3xl font-bold underline">
+      Hello Heionim! ☠️
+    </h1>
+  );
 }
 
 export default App;
